e1d069c0741e2fcc5a1a9b2b5994d1b03080f15d
Kiyodo <tyler.odo@gmail.com>
2023-11-07 19:36:17 -0800

    added functionality to search bar and recent city buttons + fixed setData() function to request 6 total days of forecast + CSS styling fixes + added comments on code functionality

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
+
 # UCI-BOOTCAMP-WEEK-6-WEATHER-DASHBOARD
+
+___
+
+## Project Description
+
+This project is a weather forecast generator that provides weather details for the current day and 5 days ahead. The user can either use the search bar to look up a city or click on a "recent city" button to select a previously chosen city.
+
+## How to run this project
+
+![Weather Dashboard - demo](<assets/images/weather dashboard - demo.png>)
+
+Click this link to visit the site:
+[Weather Dashboard](https://kiyodosan.github.io/UCI-BOOTCAMP-WEEK-6-WEATHER-DASHBOARD/)
+
+### Method 1: Search bar
+
+Click on the search bar. Type in a city name. Click on the search icon (to the right of the search bar) to generate a weather forecast for that city. Each time a new city is selected, a "recent city" button will be generated.
+
+### Method 2: Recent button
+
+If the user has already input cities using the search bar method, "recent city" buttons will generate in the "Recent" section below the search bar. Clicking on a recent button will generate a weather forecast for the city listed on the button.
+
+## How to use this project
+
+The user can interact with the weather dashboard by using either the search bar or recent button method (see "How to run this project"). Each time a recent button is created using the search bar method, the city is stored in local storage. The 5 most recent cities forecasted are stored in an array at the beginning of the local storage list. This array is used to manipulate the list of recent cities.
+
+The user can scroll through the page to view weather cards for the current day and a 5-day forecast.
+
+List of weather card info:
+* City Name
+* Forecast date
+* Weather icon based on weather description
+* Weather in degrees fahrenheit
+* Humidity %
+* Wind speed in mph
+
+### Error Handling
+- When a user inputs a value that is not a city, the weather dashboard will cancel execution and maintain the current page state. The value will not be stored in local storage.
+
+- When a duplicate search input value is selected, the value will not be stored in local storage. If it is not one of the 5 most recent cities, then it is added to the recent cities array.
+
+- When the search input value is equal to one of the 5 most recent cities, its value will not be stored in local storage.
+
+- When the array of recent cities reaches 5 elements, the least recent city is removed to make room for a new one, maintaining a consistent list of 5 cities.
+
+## Credits
+
+Tyler Odo
+
+Get all values from local storage:
+https://stackoverflow.com/questions/17745292/how-to-retrieve-all-localstorage-items-without-knowing-the-keys-in-advance
+
+Weather card styling references:
+https://www.youtube.com/watch?v=MIYQR-Ybrn4
+
+Capitalize the first letter of each word:
+https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+
+
+## License
+
+Default
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,117 @@
-const searchBtn = document.querySelector("#searchBtn");
+// City of Los Angeles selected as default
+// Arr[0] starts at 06:00 and a whole 24 hours passes at Arr[8]
+let reqUrl = "https://api.openweathermap.org/data/2.5/forecast?q=los+angeles&appid=f6eb6271b9295e567afce5c507e2465d&units=imperial";
+const reqUrlPt1 = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const reqUrlPt2 = "&appid=f6eb6271b9295e567afce5c507e2465d&units=imperial";
 
+const sidebarId = document.querySelector("#sidebarPanel");
 const cityId = document.querySelector("#cityDisplay");
 const forecastId = document.querySelector("#forecastDisplay");
-let idIndex = 0;
-
-const currentDate = dayjs().format("dddd, MMM D YYYY");
-const dateUrlPt1 = dayjs().format("YYYY-MM-");
-const dateUrlPt2 = dayjs().format("DD");
-
 let cityName = "";
+
+let idIndex = 0;
+let dayIndex = 0;
+
+const dayObj = dayjs();
+const currentDay = dayObj.format("dddd, MMM D YYYY");
 
 getCity();
 
 // Activates upon clicking the search icon. Uses form input to select which city's weather forecast will be displayed.
 function getCity() {
+  displayBtn();
+
   const inputField = document.querySelector("#inputField");
   let cityInput = "";
   let cityPlus = "";
 
-  // City of Los Angeles selected as default
-  let reqUrl = "https://api.openweathermap.org/data/2.5/weather?q=los+angeles&appid=f6eb6271b9295e567afce5c507e2465d&units=imperial&date=";
-  const reqUrlPt1 = "https://api.openweathermap.org/data/2.5/weather?q=";
-  const reqUrlPt2 = "&appid=f6eb6271b9295e567afce5c507e2465d&units=imperial&date=";
+  const searchBtn = document.querySelector("#searchBtn");
 
   searchBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    cityInput = inputField.value;
+    cityInput = inputField.value.trim();
     cityName = firstLetterCap(cityInput);
-    cityPlus = cityName.toLowerCase().replace(" ", "+");
+    cityPlus = cityName.toLowerCase().replaceAll(" ", "+");
     console.log(cityPlus);  /////////////////// Test
 
-    // Add recent location button to sidebar using cityName
-
-    removeCards();
-
     reqUrl = reqUrlPt1 + cityPlus + reqUrlPt2;
-    getWeather(reqUrl + dateUrlPt1 + dateUrlPt2);
+    getWeather(reqUrl);
   });
 }
 
+// Removes sidebar buttons (if any) then displays new buttons in order by most recent.
+function displayBtn() {
+  while (sidebarId.childElementCount > 1) {
+    sidebarId.removeChild(sidebarId.lastChild);
+  }
+
+  let btnStack = JSON.parse(localStorage.getItem("recentList"));
+
+  if (btnStack === null) {
+    return;
+  } else {
+    for (let i = btnStack.length - 1; i >= 0; i--) {
+      genBtn(btnStack[i], i);
+    }
+  }
+}
+
+// Creates a new button for the sidebar.
+function genBtn(btnStackPos, index) {
+  let newRecentBtn = document.createElement("button");
+  newRecentBtn.setAttribute("type", "button");
+  newRecentBtn.setAttribute("id", "recentBtn" + index);
+  newRecentBtn.textContent = btnStackPos;
+
+  newRecentBtn.addEventListener("click", function (event) {
+    let clickText = event.target.textContent;
+    cityName = firstLetterCap(clickText);
+    let cityPlus = clickText.toLowerCase().replaceAll(" ", "+");
+    
+    reqUrl = reqUrlPt1 + cityPlus + reqUrlPt2;
+    getWeather(reqUrl);
+  });
+
+  sidebarId.appendChild(newRecentBtn);
+}
+
+// Changes the first letter of each word to uppercase.
+// https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+function firstLetterCap(sentence) {
+  const words = sentence.split(" ");
+  
+  for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+
+  return words.join(" ");
+}
+
+// Fetches weather data from the API and generates weather cards based on the info.
+async function getWeather(link) {
+  try {
+    let response = await fetch(link);
+    if (response.ok) {
+      removeCards();
+
+      let data = await response.json();
+      console.log(data);  /////////////////// Test
+  
+      btnStorage();
+      genCard(cityId);
+      setData(data, 0);
+  
+      for (let i = 1; i < 6; i++) {
+        genCard(forecastId);
+        setData(data, i);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 // Removes all cards from the main container.
-// Resets idIndex to 0.
+// Resets idIndex and dayIndex to 0.
 function removeCards() {
   if (cityId.childElementCount > 1) {
     cityId.innerHTML = "";
@@ -57,6 +128,60 @@ function removeCards() {
   }
 
   idIndex = 0;
+  dayIndex = 0;
+}
+
+// Checks local storage for existing keys.
+// If city name is already listed, then this function cancels.
+// If city name is not yet listed, then it is added to local storage.
+// New cities are added to the btnStack. Only the 5 most recent buttons are kept.
+function btnStorage() {
+  let keys = getAllStorage();
+  console.log(keys);  /////////////////// Test
+
+  if (keys.includes(cityName)) {
+    return;
+  } else {
+    let btnIndex = keys.length;
+
+    // Create btnStack array if local storage is empty.
+    if (btnIndex === 0) {
+      let newStack = [];
+      localStorage.setItem("recentList", JSON.stringify(newStack));
+    } else if (btnIndex >=2) {
+        btnIndex--;
+    }
+
+    let btnStack = JSON.parse(localStorage.getItem("recentList"));
+
+    if (btnIndex < 5) {
+      btnStack.push(cityName);
+      localStorage.setItem("city" + btnIndex, cityName);
+      localStorage.setItem("recentList", JSON.stringify(btnStack));
+    } else {
+      btnStack.shift();
+      btnStack.push(cityName);
+      localStorage.setItem("city" + btnIndex, cityName);
+      localStorage.setItem("recentList", JSON.stringify(btnStack));
+    }
+    console.log(btnStack);  /////////////////// Test
+  }
+
+  displayBtn();
+}
+
+// Get all values from local storage and put them in an array.
+// https://stackoverflow.com/questions/17745292/how-to-retrieve-all-localstorage-items-without-knowing-the-keys-in-advance
+function getAllStorage() {
+  let values = [];
+  let keys = Object.keys(localStorage);
+  let i = keys.length;
+
+  while (i--) {
+    values.push(localStorage.getItem(keys[i]));
+  }
+
+  return values;
 }
 
 // Creates a new weather card with unique IDs.
@@ -75,11 +200,12 @@ function genCard(display) {
 
   let h3Date = document.createElement("h3");
   h3Date.classList.add("date");
-  h3Date.textContent = currentDate;
+  h3Date.setAttribute("id", "date" + idIndex);
+  h3Date.textContent = currentDay;
 
   let imgWeatherIcon = document.createElement("img");
   imgWeatherIcon.classList.add("weather-icon");
-  imgWeatherIcon.setAttribute("src", "./assets/images/clouds.png"); /////////////////// Must choose icon based on weather
+  imgWeatherIcon.setAttribute("src", "./assets/images/clouds.png");
   imgWeatherIcon.setAttribute("alt", "weather icon");
   imgWeatherIcon.setAttribute("id", "weatherIcon" + idIndex);
 
@@ -151,88 +277,15 @@ function genCard(display) {
   idIndex++;
 }
 
-// Changes the first letter of each word to uppercase.
-// https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
-function firstLetterCap(sentence) {
-  const words = sentence.split(" ");
-  
-  for (let i = 0; i < words.length; i++) {
-      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-  }
-
-  return words.join(" ");
-}
-
-/* // Fetches weather data from the API and generates weather cards based on the info.
-function getWeather(link) {
-  fetch(link).then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    console.log(data);  /////////////////// Test
-
-    genCard(cityId);
-    setData(data, 0);
-
-    let nextDate = "";
-
-    for (let i = 1; i < 6; i++) {
-      nextDate = dateUrlPt2.add(i, "day");
-      weatherData = getWeather(reqUrl + dateUrlPt1 + nextDate);
-      genCard(forecastId);
-      setData(weatherData, i);
-    }
-
-    // return data;  // Maybe don't have to return anything if card is already generated
-  }).catch(function(err) {
-    console.log(err);
-    // Returning 0 to a boolean value for checking success or failure
-    return 0;
-  });
-} */
-
-// Fetches weather data from the API and generates weather cards based on the info.
-async function getWeather(link) {
-  try{
-    let response = await fetch(link);
-    let data = await response.json();
-    console.log(data);  /////////////////// Test
-
-    genCard(cityId);
-    setData(data, 0);
-
-    let nextDate = "";
-    let nextResponse = "";
-    let nextData = "";
-
-    for (let i = 1; i < 6; i++) {
-      nextDate = dateUrlPt2.add(i, "day");
-
-
-      /////////////////// Modify this section to use nextDate, nextResponse, and nextData for new fetch requests
-      nextResponse = await getWeather(reqUrl + dateUrlPt1 + nextDate);
-      genCard(forecastId);
-      setData(nextResponse, i);
-
-
-    }
-
-  } catch (err) {
-    console.log(err);
-  }
-}
-
 // Adds weather data to a weather card.
 function setData (weather, index) {
   let cityClass = document.querySelector("#city" + index);
-
-/*   // Only changes if URL has temp info
-  if (weather.main.temp > -1000) {
-    cityClass.textContent = cityName;
-  } */
-
   cityClass.textContent = cityName;
 
-  let switchIcon = weather.weather[0].main;
+  let dateId = document.querySelector("#date" + index);
+  dateId.textContent = dayObj.add(index, "day").format("dddd, MMM D YYYY");
+
+  let switchIcon = weather.list[dayIndex].weather[0].main;
   let weatherIconId = document.querySelector("#weatherIcon" + index);
 
   // Changes weather icon based on weather
@@ -258,12 +311,17 @@ function setData (weather, index) {
   }
 
   let tempId = document.querySelector("#temp" + index);
-  console.log("Temp ID: " + tempId);  /////////////////// Test
-  tempId.textContent = weather.main.temp.toFixed(0) + " °F";
+  tempId.textContent = weather.list[dayIndex].main.temp.toFixed(0) + " °F";
 
   let humidityId = document.querySelector("#humidity" + index);
-  humidityId.textContent = weather.main.humidity + " %";
+  humidityId.textContent = weather.list[dayIndex].main.humidity + " %";
 
   let windId = document.querySelector("#wind" + index);
-  windId.textContent = weather.wind.speed.toFixed(0) + " mph";
+  windId.textContent = weather.list[dayIndex].wind.speed.toFixed(0) + " mph";
+
+  if (dayIndex === 0) {
+    dayIndex += 7;
+  } else if (dayIndex >= 7) {
+    dayIndex += 8;
+  }
 }
